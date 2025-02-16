8c5848529a781d1ea2f83fd10f4b694d0af1632d
stecyuk8899 <stecyuk8899@ukr.net>
2025-02-16 15:29:03 +0200

    Fix errors in calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 import sys
-
 from PyQt6.QtCore import Qt
 from functools import partial
 from PyQt6.QtWidgets import (
@@ -21,62 +20,68 @@ ERROR_MSG = "ERROR"
 class PyCalcWindow(QMainWindow):
     """PyCalc's main window (GUI or view)."""
 
-    """PyCalc's main window (GUI or view)."""
-
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyCalc")
         self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
         self.generalLayout = QVBoxLayout()
+
+        # Set up central widget
         centralWidget = QWidget(self)
         centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
+
         self._createDisplay()
         self._createButtons()
 
-        def _createDisplay(self):
-            self.display = QLineEdit()
-            self.display.setFixedHeight(DISPLAY_HEIGHT)
-            self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
-            self.display.setReadOnly(True)
-            self.generalLayout.addWidget(self.display)
+    def _createDisplay(self):
+        """Create the display."""
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_HEIGHT)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
 
-        def _createButtons(self):
-            self.buttonMap = {}
-            buttonsLayout = QGridLayout()
-            keyBoard = [
-                ["7", "8", "9", "/", "C"],
-                ["4", "5", "6", "*", "("],
-                ["1", "2", "3", "-", ")"],
-                ["0", "00", ".", "+", "="],
-            ]
-            for row, keys in enumerate(keyBoard):
-                for col, key in enumerate(keys):
-                    self.buttonMap[key] = QPushButton(key)
-                    self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
-                    buttonsLayout.addWidget(self.buttonMap[key], row, col)
-            self.generalLayout.addLayout(buttonsLayout)
+    def _createButtons(self):
+        """Create the calculator's buttons."""
+        self.buttonMap = {}
+        buttonsLayout = QGridLayout()
+        keyBoard = [
+            ["7", "8", "9", "/", "C"],
+            ["4", "5", "6", "*", "("],
+            ["1", "2", "3", "-", ")"],
+            ["0", "00", ".", "+", "="],
+        ]
+        for row, keys in enumerate(keyBoard):
+            for col, key in enumerate(keys):
+                self.buttonMap[key] = QPushButton(key)
+                self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                buttonsLayout.addWidget(self.buttonMap[key], row, col)
+        self.generalLayout.addLayout(buttonsLayout)
 
-        def setDisplayText(self, text):
-            """Set the display's text."""
-            self.display.setText(text)
-            self.display.setFocus()
+    def setDisplayText(self, text):
+        """Set the display's text."""
+        self.display.setText(text)
+        self.display.setFocus()
 
-        def displayText(self):
-            """Get the display's text."""
-            return self.display.text()
+    def displayText(self):
+        """Get the display's text."""
+        return self.display.text()
 
-        def clearDisplay(self):
-            """Clear the display."""
-            self.setDisplayText("")
+    def clearDisplay(self):
+        """Clear the display."""
+        self.setDisplayText("")
+
 
 def evaluateExpression(expression):
     """Evaluate an expression (Model)."""
     try:
+        # Evaluate the given expression safely
         result = str(eval(expression, {}, {}))
     except Exception:
         result = ERROR_MSG
     return result
+
 
 class PyCalc:
     """PyCalc's controller class."""
@@ -87,16 +92,19 @@ class PyCalc:
         self._connectSignalsAndSlots()
 
     def _calculateResult(self):
+        """Evaluate expressions and display results."""
         result = self._evaluate(expression=self._view.displayText())
         self._view.setDisplayText(result)
 
     def _buildExpression(self, subExpression):
+        """Build the expression to be evaluated."""
         if self._view.displayText() == ERROR_MSG:
             self._view.clearDisplay()
         expression = self._view.displayText() + subExpression
         self._view.setDisplayText(expression)
 
     def _connectSignalsAndSlots(self):
+        """Connect signals and slots."""
         for keySymbol, button in self._view.buttonMap.items():
             if keySymbol not in {"=", "C"}:
                 button.clicked.connect(
@@ -105,6 +113,7 @@ class PyCalc:
         self._view.buttonMap["="].clicked.connect(self._calculateResult)
         self._view.display.returnPressed.connect(self._calculateResult)
         self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+
 
 def main():
     """PyCalc's main function."""
