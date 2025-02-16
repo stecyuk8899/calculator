bbdc9dc0edbc36fe8b0cef4222f436d214f269eb
stecyuk8899 <stecyuk8899@ukr.net>
2025-02-16 14:58:08 +0200

    Add skeleton and view in calculator.py

---
+++ calculator.py
@@ -0,0 +1,82 @@
+import sys
+
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+WINDOW_SIZE = 235
+DISPLAY_HEIGHT = 35
+BUTTON_SIZE = 40
+
+
+class PyCalcWindow(QMainWindow):
+    """PyCalc's main window (GUI or view)."""
+
+    """PyCalc's main window (GUI or view)."""
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("PyCalc")
+        self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
+        centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
+        self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
+
+        def _createDisplay(self):
+            self.display = QLineEdit()
+            self.display.setFixedHeight(DISPLAY_HEIGHT)
+            self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+            self.display.setReadOnly(True)
+            self.generalLayout.addWidget(self.display)
+
+        def _createButtons(self):
+            self.buttonMap = {}
+            buttonsLayout = QGridLayout()
+            keyBoard = [
+                ["7", "8", "9", "/", "C"],
+                ["4", "5", "6", "*", "("],
+                ["1", "2", "3", "-", ")"],
+                ["0", "00", ".", "+", "="],
+            ]
+            for row, keys in enumerate(keyBoard):
+                for col, key in enumerate(keys):
+                    self.buttonMap[key] = QPushButton(key)
+                    self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                    buttonsLayout.addWidget(self.buttonMap[key], row, col)
+            self.generalLayout.addLayout(buttonsLayout)
+
+        def setDisplayText(self, text):
+            """Set the display's text."""
+            self.display.setText(text)
+            self.display.setFocus()
+
+        def displayText(self):
+            """Get the display's text."""
+            return self.display.text()
+
+        def clearDisplay(self):
+            """Clear the display."""
+            self.setDisplayText("")
+
+
+
+def main():
+    """PyCalc's main function."""
+    pycalcApp = QApplication([])
+    pycalcWindow = PyCalcWindow()
+    pycalcWindow.show()
+    sys.exit(pycalcApp.exec())
+
+
+if __name__ == "__main__":
+    main()
